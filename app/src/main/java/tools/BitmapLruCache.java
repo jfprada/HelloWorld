919ecc64e14cc6173074368edcd03488bc6e13d0
jfprada <juanfprada@gmail.com>
2017-03-07 16:06:50 -0500

    tools started

---
+++ app/src/main/java/tools/BitmapLruCache.java
@@ -0,0 +1,34 @@
+package tools;
+
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.toolbox.ImageLoader;
+
+/**
+ * Created by JFPrada on 3/7/17.
+ */
+
+public class BitmapLruCache extends LruCache<String, Bitmap> implements ImageLoader.ImageCache{
+    public BitmapLruCache(int maxSize) {
+        super(maxSize);
+    }
+
+
+    @Override
+    protected int sizeOf(String key, Bitmap value) {
+        return value.getRowBytes() * value.getHeight();
+    }
+
+
+    @Override
+    public Bitmap getBitmap(String url) {
+        return get(url);
+    }
+
+
+    @Override
+    public void putBitmap(String url, Bitmap bitmap) {
+        put(url, bitmap);
+    }
+}
